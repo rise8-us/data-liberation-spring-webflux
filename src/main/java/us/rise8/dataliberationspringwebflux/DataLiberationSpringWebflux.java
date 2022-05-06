d57a952e6fba577be7d9e61f118966f8d6e553e8
Jeffrey Wills <jwills@rise8.us>
2022-05-06 12:57:53 -0400

    init/ add the spring initiliz project

---
+++ src/main/java/us/rise8/dataliberationspringwebflux/DataLiberationSpringWebflux.java
@@ -0,0 +1,13 @@
+package us.rise8.dataliberationspringwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLiberationSpringWebflux {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLiberationSpringWebflux.class, args);
+	}
+
+}
---
+++ src/test/java/us/rise8/dataliberationspringwebflux/DataLiberationSpringWebfluxTests.java
@@ -0,0 +1,13 @@
+package us.rise8.dataliberationspringwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLiberationSpringWebfluxTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
